6010d848f344ce1cd947b074c556536b1052051a
Rajdip Khavad <89386681+rajdipsimform@users.noreply.github.com>
2023-04-24 12:56:34 +0530

    Add project files.

---
+++ IdentityServer4DemoMVC/Service/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using IdentityModel.Client;
+
+namespace IdentityServer4DemoMVC.Service
+{
+    public interface ITokenService
+    {
+        Task<TokenResponse> GetToken(string scope);
+    }
+}
---
+++ IdentityServer4Server/IdentityConfiguration.cs
@@ -0,0 +1,59 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Security.Claims;
+
+namespace IdentityServer4Server
+{
+    public class IdentityConfiguration
+    {
+        public static List<TestUser> TestUsers => new List<TestUser>{
+            new TestUser
+            {
+                SubjectId = "1144",
+                Username = "rajdip",
+                Password = "rajdip",
+                Claims =
+                {
+                    new Claim(JwtClaimTypes.Name, "Rajdip Khavad"),
+                    new Claim(JwtClaimTypes.GivenName, "Rajdip"),
+                    new Claim(JwtClaimTypes.FamilyName, "Rajdip"),
+                    new Claim(JwtClaimTypes.WebSite, "https://www.simform.com/"),
+                }
+            }
+        };
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("myApi.read"),
+                new ApiScope("myApi.write"),
+            };
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+                new ApiResource("myApi")
+                {
+                    Scopes = new List<string>{ "myApi.read","myApi.write" },
+                    ApiSecrets = new List<Secret>{ new Secret("supersecret".Sha256()) }
+                }
+            };
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "cwm.client",
+                    ClientName = "Client Credentials Client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedScopes = { "myApi.read" }
+                },
+            };
+    }
+}
